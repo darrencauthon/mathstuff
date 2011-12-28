325faa5a88dfdb5b43f6bfc906faa2f0b0f5d666
Darren Cauthon <darren@cauthon.com>
2011-12-27 21:02:53 -0600

    Handle more sum situations.

--- mathstuff.js
+++ mathstuff.js
@@ -1,7 +1,11 @@
 (function( $ ) {
   $.fn.sum  = function() {  
     if (this.length == 0) return 0;
-    var value = this.val();
-    return isNaN(value) ? 0 : value;
+    var value = 0;
+    this.each(function() {
+      if (isNaN($(this).val()) == false)
+        value = value + parseFloat($(this).val());
+    });
+    return value;
   };
 })( jQuery );
